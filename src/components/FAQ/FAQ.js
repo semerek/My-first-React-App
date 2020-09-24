0c82daef18024a52a7220d1667faf2b819d5add4
Marta Semrau <mm.semrau@gmail.com>
2020-09-24 11:07:53 +0200

    14.2 Add styles

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { pageFAQ } from '../../data/dataStore';
 import Container from '../Container/Container';
 import Hero from '../Hero/Hero.js';
-
+import styles from './FAQ.scss';
 
 
 const FAQ = () => (
   <Container>
-    <section>
+    <section className={styles.component}>
       <Hero titleText={pageFAQ.title} image={pageFAQ.image} />
       <p>{pageFAQ.content}</p>
     </section>
